590d1bcdc758e1cdacd69cbf2961dd690a1e2b50
Adarsh Singh <Adarshsingh933@gmail.com>
2023-06-24 10:44:36 +0530

    create user sign-up

--- codeial/controllers/user_controller.js
+++ codeial/controllers/user_controller.js
@@ -1,3 +1,36 @@
+const User=require('../models/user');
+
 module.exports.profile=function(req,res){
     return res.render('user_profile');
+}
+module.exports.signUp=function(req,res){
+    return res.render('signUp');
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('signIn');
+}
+
+module.exports.create = async function (req, res) {
+    console.log(req.body);
+    if (req.body.password !== req.body['confirmed-password']) {
+      return res.redirect('back');
+    }
+  
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        await User.create(req.body);
+        return res.redirect('/user/sign-in');
+      } else {
+        return res.redirect('back');
+      }
+    } catch (err) {
+      console.log('Error in creating user', err);
+      return res.redirect('back');
+    }
+  };
+
+module.exports.createSession=function(req,res){
+    return res.render('signIn');
 }
--- codeial/routes/user.js
+++ codeial/routes/user.js
@@ -4,5 +4,8 @@ const router=express.Router();
 const usercontroller=require('../controllers/user_controller');
 console.log('user loaded');
 router.get('/profile',usercontroller.profile);
+router.get('/sign-up',usercontroller.signUp);
+router.get('/sign-in',usercontroller.signIn);
+router.post('/create',usercontroller.create);
 
 module.exports=router;
